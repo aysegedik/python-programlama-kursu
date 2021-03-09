adb13227e86b923d113d0a0862a4c6139f1886e8
aysegedik <80269711+aysegedik@users.noreply.github.com>
2021-03-09 19:47:12 +0300

    fonksiyon

---
+++ fonksiyon1.py
@@ -0,0 +1,19 @@
+# def fonk_adi():
+#   kodlar...
+
+def toplama():
+    a=5
+    b=6
+    sonuc= a+b
+    print(sonuc)
+
+def cikarma():
+    print("Çıkarma fonk. çalıştı.")
+
+def carpma():
+    pass
+
+toplama()
+toplama()
+cikarma()
+cikarma()
